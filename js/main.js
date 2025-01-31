daa84f229d19bd323af22a6e598c9e805173de7a
Akane <akanes.phone@gmail.com>
2025-01-31 23:19:56 +0900

    サイドバー開閉の記述

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@
 //open
 $('.c-header__btn').on('click',function(){
     //sidebarを開く
-    $('.c-sidebar__bg').addClass('open');
+    $('.p-sidebar__bg').addClass('open');
+    $('.c-sidebar').addClass('open');
+    $('.l-sidebar').addClass('open');
     //btnの削除
     $(this).addClass('open');
 });
@@ -10,7 +12,12 @@ $('.c-header__btn').on('click',function(){
 //close
 $('.c-sidebar__btn').on('click',function(){
     //sidebarを閉じる
-    $('.c-sidebar__bg').removeClass('open');
+    $('.p-sidebar__bg').removeClass('open');
+    $('.c-sidebar').removeClass('open');
     //btn
     $('.c-header__btn').removeClass('open');
+});
+
+setTimeout(function(){
+    $('.l-sidebar').removeClass('open');
 });
